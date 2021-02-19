8aa03de039a0f095d3e6685c34ef6b490c67a0ac
EvgenyShchurkin <jeyshch@gmail.com>
2021-02-19 12:20:01 +0300

    cleaning Scanner buffer added

--- DavidLesson_3/src/main/java/ThirdHomework.java
+++ DavidLesson_3/src/main/java/ThirdHomework.java
@@ -4,39 +4,40 @@ import java.util.Scanner;
 public class ThirdHomework {
     public static void main(String[] args) {
 
-        String[] words = new String[] {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+        String[] words =  {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
                     "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
                     "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
         Random rand = new Random();
         String chosenWord = words[rand.nextInt(words.length)];
         System.out.println("Компьютер загадал слово.\nВам нужно это слово отгадать.\n");
-//        System.out.println(chosenWord);
+//        System.out.println(chosenWord);   //для теста
         Scanner in = new Scanner(System.in);
         boolean isFound = false;
         do {
             System.out.print("Введите слово: ");
             String word = in.next();
+            in.nextLine();              // для очистки буфера, если слова вводились через пробел
             if (word.equals(chosenWord)) {
                 System.out.println("Вы угадали!!! Игра окончена!");
                 isFound = true;
-                in.close();
             } else {
                 System.out.println("Неправильно");
                 PrintCorrectSymbols(chosenWord, word);
             }
         } while (!isFound);
+        in.close();
     }
 
-    public static void PrintCorrectSymbols(String correctWord, String yourWord){
-        for (int i = 0; i < Math.min(correctWord.length(),yourWord.length()); i++) {
-            if (yourWord.charAt(i) == correctWord.charAt(i)) {
+    public static void PrintCorrectSymbols(String correctWord, String inputWord){
+        for (int i = 0; i < Math.min(correctWord.length(),inputWord.length()); i++) {
+            if (inputWord.charAt(i) == correctWord.charAt(i)) {
                 System.out.print(correctWord.charAt(i));
             } else {
                 System.out.print('#');
             }
         }
-        for (int i = 0; i <15-Math.min(correctWord.length(),yourWord.length()) ; i++) {     //допечатать ###
+        for (int i = 0; i <15-Math.min(correctWord.length(),inputWord.length()) ; i++) {     //допечатать ###
             System.out.print("#");
         }
         System.out.println("\n");
